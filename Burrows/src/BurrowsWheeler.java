211f10871675584c59edaf86f27aca200ffc9d45
Yap Wooi Hen <wooihen@live.com>
2014-05-24 14:35:41 +0800

    Decoder functionality
    Added Burrows-Wheeler decoding functionality

--- Burrows/src/BurrowsWheeler.java
+++ Burrows/src/BurrowsWheeler.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class BurrowsWheeler {
+	private static final int R = 256;
+	
 	// apply Burrows-Wheeler encoding, reading from standard input and writing to standard output
 	public static void encode() {
 		StringBuilder sb = new StringBuilder();
@@ -44,11 +50,44 @@ public class BurrowsWheeler {
 	
 	// apply Burrows-Wheeler decoding, reading from standard input and writing to standard output
 	public static void decode() {
+		// Read first
+		int first = BinaryStdIn.readInt();
+		StringBuilder sb = new StringBuilder();
+		while( !BinaryStdIn.isEmpty() ) {
+			sb.append( BinaryStdIn.readChar() );
+		}
 		
+		// Sort suffixes
+		String s = sb.toString();
+		char[] sorted = s.toCharArray();
+		Arrays.sort(sorted);
+		
+		// Construct the next[] array from output stream
+		int[] next = new int[s.length()];
+		
+		// FIFO O(1) runtime 		
+		Map<Character,LinkedList<Integer>> q = new HashMap<Character,LinkedList<Integer>>();
+		for(int i=0; i<s.length(); i++) {
+			if( !q.containsKey(s.charAt(i)) ) {
+				q.put(s.charAt(i), new LinkedList<Integer>());
+			}
+			q.get(s.charAt(i)).addLast(i);
+		}
+		
+		for(int i=0; i<s.length(); i++) {
+			next[i] = q.get(sorted[i]).pop();
+		}
+		
+		// Decode the encoded message
+		for(int i=0; i < s.length(); i++) {
+			BinaryStdOut.write(sorted[first]);
+			first = next[first];
+		}
+		BinaryStdOut.flush();
 	}
 	
 	// if args[0] is '-', apply Burrows-Wheeler encoding
-	// if args[0] is '-', apply Burrows-Wheeler decoding
+	// if args[0] is '+', apply Burrows-Wheeler decoding
 	public static void main(String[] args) {
 		if(args.length < 1) {
 			throw new IllegalArgumentException("Usage: BurrowsWheeler -|+");
